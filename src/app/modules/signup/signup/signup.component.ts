1493c578a395b55890b21bcb23abcee663244969
van_da <iivnnn123@gmail.com>
2024-07-23 02:29:39 -0500

    second test mejora vista

--- src/app/modules/signup/signup/signup.component.ts
+++ src/app/modules/signup/signup/signup.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-
+  private isProducerSignup:Boolean=false
+  private isAdminSignup:Boolean=false
+  changeProducer(){
+   this.isProducerSignup= !this.isProducerSignup;
+   this.isAdminSignup= false;
+  } 
+  changeAdmin(){
+   this.isAdminSignup= !this.isAdminSignup;
+   this.isProducerSignup= false;
+  }
+  get producerSignup(){
+    return this.isProducerSignup;
+  }
+  get adminSignup(){
+    return this.isAdminSignup;
+  }
 }
