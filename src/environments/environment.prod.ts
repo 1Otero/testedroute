006c00a230af0cc7ce357cbb8899c3d493ef216c
van_da <iivnnn123@gmail.com>
2024-10-01 02:06:54 -0500

    test environment test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     "production": true,
-    "urllocal": "https://vanportafolioback.netlify.app"
+    // "urllocal": "https://vanportafolioback.netlify.app"
+    "urllocal": "http://44.211.168.70:8082"
 };
